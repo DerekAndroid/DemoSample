91fb8162d8eac01401ef4c1a5fa794cbffe64c1b
derek_chang <derek_chang@friday.tw>
2017-11-07 15:21:27 +0800

    增加一些測試

--- app/src/main/java/com/kered/demosample/MainActivity.java
+++ app/src/main/java/com/kered/demosample/MainActivity.java
@@ -2,9 +2,8 @@ package com.kered.demosample;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,15 +11,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.kered.dklog.DKLog;
+import com.kered.dklog.Trace;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button mButton;
+
+    private int newValue = 500;
+    private static int value = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        DKLog.setEnable(true);
+
+        mButton = (Button) findViewById(R.id.button6);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DKLog.d(TAG, Trace.getCurrentMethod() + String.valueOf(value) + newValue);
+            }
+        });
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
